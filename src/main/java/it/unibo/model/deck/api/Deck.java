c726c49897c6f187553a594e255d7ee6b393c4dc
Martino Pagliarani <martino.pagliarani@studio.unibo.it>
2023-05-13 18:52:13 +0200

    [ADD] added Javadoc for Deck

--- src/main/java/it/unibo/model/deck/api/Deck.java
+++ src/main/java/it/unibo/model/deck/api/Deck.java
@@ -1,17 +1,32 @@
 package it.unibo.model.deck.api;
 
-import java.util.LinkedList;
-
+import java.util.List;
+/**
+ * Represents a generic deck of cards.
+ * 
+ * @param <T> the type of cards in the deck.
+ */
 public interface Deck<T> {
-
+    /**
+     * Adds a card to the deck.
+     * 
+     * @param card the card to be added
+     */
     void addCard(T card);
-
+    /**
+     * Draws a card from the deck.
+     * 
+     * @return the card drawn
+     */
     T drawCard();
-
+    /**
+     * Shuffles the deck.
+     */
     void shuffle();
-
-    LinkedList<T> getDeck();
-
-    String toString();
-
+    /**
+     * Gets the deck.
+     * 
+     * @return the deck
+     */
+    List<T> getDeck();
 }
