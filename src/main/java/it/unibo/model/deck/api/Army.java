aa5741c518076a3c9b282964d2e0c2f158baa218
Martino Pagliarani <martino.pagliarani@studio.unibo.it>
2023-05-13 18:49:42 +0200

    [ADD] added Javadoc for Army and ArmyImpl

--- src/main/java/it/unibo/model/deck/api/Army.java
+++ src/main/java/it/unibo/model/deck/api/Army.java
@@ -1,13 +1,37 @@
 package it.unibo.model.deck.api;
-
+/**
+ * Represents an Army card.
+ */
 public interface Army {
-
+    /**
+     * Represents the type of the Army card.
+     */
     enum ArmyType {
-        INFANTRY, CAVALRY, ARTILLERY
+        /**
+         * Represents the Infantry Army card.
+         */
+        INFANTRY,
+        /**
+         * Represents the Cavalry Army card.
+         */
+        CAVALRY,
+        /**
+         * Represents the Artillery Army card.
+         */
+        ARTILLERY
     }
-
+    /**
+     * Gets the type of the Army card.
+     * 
+     * @return the type of the Army card.
+     */
     ArmyType getArmyType();
-
+    /**
+     * Gets the string representation of the Army card.
+     * 
+     * @return the string representation of the Army card.
+     */
+    @Override
     String toString();
 
 }
--- src/main/java/it/unibo/model/deck/impl/ArmyImpl.java
+++ src/main/java/it/unibo/model/deck/impl/ArmyImpl.java
@@ -1,20 +1,34 @@
 package it.unibo.model.deck.impl;
 
 import it.unibo.model.deck.api.Army;
-
+/**
+ * Represents an Army card.
+ */
 public class ArmyImpl implements Army {
 
     private final ArmyType armyType;
-
+    /**
+     * Creates an Army card.
+     * 
+     * @param armyType the type of the Army card.
+     */
     public ArmyImpl(final ArmyType armyType) {
         this.armyType = armyType;
     }
-
+    /**
+     * Gets the type of the Army card.
+     * 
+     * @return the type of the Army card.
+     */
     @Override
     public ArmyType getArmyType() {
         return armyType;
     }
-
+    /**
+     * Gets the string representation of the Army card.
+     * 
+     * @return the string representation of the Army card.
+     */
     @Override
     public String toString() {
         return this.armyType.toString();
