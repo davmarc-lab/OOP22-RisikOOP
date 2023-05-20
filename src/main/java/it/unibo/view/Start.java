32bffdf6e0abbe5879120af138f1c671dcb9d06b
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2023-05-20 12:04:27 +0200

    [ADD] main class of application

---
+++ src/main/java/it/unibo/view/Start.java
@@ -0,0 +1,12 @@
+package it.unibo.view;
+
+import it.unibo.view.game_screen.GameFrame;
+
+/**
+ * Main class of application.
+ */
+public class Start {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
