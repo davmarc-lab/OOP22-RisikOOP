4ad96c4e5ed74516f6a7de4f230a756933f59cfd
Mattia Forti <mattia.forti2@studio.unibo.it>
2023-05-20 10:18:08 +0200

    [MOD] Fixed some typos and improved constructor

--- src/main/java/it/unibo/model/game_loop/impl/TurnManagerImpl.java
+++ src/main/java/it/unibo/model/game_loop/impl/TurnManagerImpl.java
@@ -1,6 +1,7 @@
 package it.unibo.model.game_loop.impl;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,13 +21,13 @@ public final class TurnManagerImpl implements TurnManager {
     private final List<Player> players;
 
     private Iterator<Player> playerIterator;
-    private Player currentPLayer;
+    private Player currentPlayer;
 
     /**
      * Creates an iterator to cycle through the player list and sets the turn order.
      * @param players a list of players.
      */
-    public TurnManagerImpl(final List<Player> players) {
+    public TurnManagerImpl(final Collection<Player> players) {
         this.players = new LinkedList<>(players);
         playerIterator = this.players.iterator();
         setRandomOrder();
@@ -36,31 +37,29 @@ public final class TurnManagerImpl implements TurnManager {
      * Randomizes the order of players based on the dice throw.
      */
     private void setRandomOrder() {
-        Dice d6 = new DiceImpl(DICE_FACES);
+        final Dice d6 = new DiceImpl(DICE_FACES);
         List<Pair<Player, Integer>> list = new ArrayList<>();
         for (int i = 0; i < players.size(); i++) {
             list.add(new PairImpl<>(players.get(i), d6.roll()));
         }
-        System.out.println(list);
         list.sort((p1, p2) -> p1.get2() < p2.get2() ? 1 : -1);
         for (int i = 0; i < players.size(); i++) {
             players.set(i, list.get(i).get1());
         }
-        System.out.println(players);
     }
 
     @Override
     public Player getCurrentPlayer() {
-        return currentPLayer;
+        return currentPlayer;
     }
 
     @Override
     public void switchToNextPlayer() {
         if (playerIterator.hasNext()) {
-            currentPLayer = playerIterator.next();
+            currentPlayer = playerIterator.next();
         } else {
             playerIterator = players.iterator();
-            currentPLayer = playerIterator.next();
+            currentPlayer = playerIterator.next();
         }
     }
 
@@ -68,7 +67,7 @@ public final class TurnManagerImpl implements TurnManager {
     public String toString() {
         Iterator<Player> it = players.iterator();
         String msg = "";
-        String separator = "----------------------\n";
+        final String separator = "----------------------\n";
         int i = 1;
         while (it.hasNext()) {
             msg += separator + "TURN " + i + "\n" + it.next().toString() + "\n";
