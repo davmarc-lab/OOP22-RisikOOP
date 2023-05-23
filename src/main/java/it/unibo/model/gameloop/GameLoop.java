8ad707b348ab8b25fd87a50cebfa0659f0ca389d
Mattia Forti <mattia.forti2@studio.unibo.it>
2023-05-23 18:12:43 +0200

    [MOD] Added controller, removed turn manager and replaced game prep with game board

--- src/main/java/it/unibo/model/gameloop/GameLoop.java
+++ src/main/java/it/unibo/model/gameloop/GameLoop.java
@@ -1,11 +1,10 @@
 package it.unibo.model.gameloop;
 
+import it.unibo.controller.api.MainController;
+import it.unibo.model.board.api.GameBoard;
+import it.unibo.model.board.impl.GameBoardImpl;
 import it.unibo.model.gameloop.api.PhaseManager;
-import it.unibo.model.gameloop.api.TurnManager;
 import it.unibo.model.gameloop.impl.PhaseManagerImpl;
-import it.unibo.model.gameloop.impl.TurnManagerImpl;
-import it.unibo.model.gameprep.api.GamePrep;
-import it.unibo.model.gameprep.impl.GamePrepImpl;
 
 /**
  * This class is used to process the input received from the view
@@ -14,49 +13,41 @@ import it.unibo.model.gameprep.impl.GamePrepImpl;
 public class GameLoop {
 
     private final PhaseManager phaseManager;
-    private final TurnManager turnManager;
-    private final GamePrep prep;
+    private final GameBoard board;
+    private final MainController controller;
 
     /**
      * Prepares all the necessary components.
      */
-    public GameLoop() {
-        prep = new GamePrepImpl();
+    public GameLoop(final MainController controller) {
+        this.controller = controller;
+        board = new GameBoardImpl();
         phaseManager = new PhaseManagerImpl();
-        turnManager = new TurnManagerImpl(prep.getPlayers());
     }
 
     /**
      * Checks the current phase of the turn and acts accordingly.
      * 
      * @param input the input received from the view, it can be a Territory, a Card
-     *              or an Integer (army)
+     *              or an Integer (troops)
      */
     public void processInput(final Object input) {
         switch (phaseManager.getCurrentPhase()) {
             case PREPARATION:
-                // TODO dare le armate al giocatore -> obj è Territory
                 phaseManager.switchToNextPhase();
                 break;
             case PLAY_CARDS:
-                // TODO il giocatore gioca le carte -> obj è Card
                 phaseManager.switchToNextPhase();
                 break;
             case COMBAT:
-                // TODO il giocatore sceglie prima un suo territorio poi uno nemico e le armate
-                // da usare -> obj è Territory o Integer
-                // TODO se il giocatore preme su "termina combattimento" passa alla fase
-                // MOVEMENT
+                phaseManager.switchToNextPhase();
                 break;
             case MOVEMENT:
-                // TODO il il giocatore sceglie 2 suoi territori e le armate da spostare -> obj
-                // = Territory o Integer
-                // TODO se il giocatore preme su "termina turno" passa alal fase END_TURN
+                phaseManager.switchToNextPhase();
                 break;
             case END_TURN:
-                // TODO pesca una carta se il player ha fatto una conquista
                 phaseManager.switchToNextPhase();
-                turnManager.switchToNextPlayer();
+                board.getTurnManager().switchToNextPlayer();
                 break;
             default:
                 break;
