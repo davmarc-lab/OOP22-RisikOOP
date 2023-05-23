261544632565e78eac592b6d161991bcf2ea9d5e
Mattia Forti <mattia.forti2@studio.unibo.it>
2023-05-23 17:09:53 +0200

    [ADD] Added MainController

---
+++ src/main/java/it/unibo/controller/api/MainController.java
@@ -0,0 +1,43 @@
+package it.unibo.controller.api;
+
+import java.util.Set;
+
+import it.unibo.model.territory.api.Territory;
+
+/**
+ * This interface models a controller that allows the board view to communicate
+ * with the model.
+ */
+public interface MainController {
+
+    /**
+     * Sends the input received from the board to the model.
+     * 
+     * @param input what the player clicked
+     */
+    void sendInput(Object input);
+
+    /**
+     * Sends a message to be displayed on the GUI.
+     * 
+     * @param message the message to be attached to the notification
+     */
+    void sendMessage(String message);
+
+    /**
+     * Disables the specified territories in the GUI.
+     * @param territories the Set of territories
+     */
+    void disableTerritories(Set<Territory> territories);
+
+    /**
+     * Enables all the territories in the GUI.
+     */
+    void enableAllTerritories();
+
+    /**
+     * Disables all the territories in the GUI.
+     */
+    void disableAllTerritories();
+
+}
