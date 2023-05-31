b1eedd43044910f7ad9141c61c5af898d9b4d74e
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2023-05-31 19:28:44 +0200

    [FIX] merge conflict completed

--- src/main/java/it/unibo/view/game_screen/api/CardZone.java
+++ src/main/java/it/unibo/view/game_screen/api/CardZone.java
@@ -1,5 +1,9 @@
 package it.unibo.view.game_screen.api;
 
+import it.unibo.controller.playerhand.api.PlayerHandController;
+
 public interface CardZone {
-    
+    void setController(PlayerHandController controller);
+
+    void updateView();
 }
